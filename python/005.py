0987a1cec3d0598eba9c4e7eef7f987b3f6b8bb0
Caitlin Schaefer <ceschae@gmail.com>
2019-09-17 16:51:48 -0700

    this doesn't work, but it's the given optimized solution

--- python/005.py
+++ python/005.py
@@ -1,15 +1,19 @@
-import sys
+import math 
 
-def divAll(n):
-    for i in range(1, 21):
-        if num % i != 0:
-            return False
-    return True
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71] # len(primes) = 20
+exps = [0 for x in range(len(primes))]
+result = 1
+i = 0
+check = True
+limit = math.sqrt(len(primes))
 
-num = 20
-for num in range(20, sys.maxsize, 20):
-    if divAll(num):
-        print(num)
-        break
-    else:
-        num += 1
+while primes[i] < len(primes):
+    exps[i] = 1
+    if check:
+        if primes[i] <= limit:
+            exps[i] = math.floor(math.log(len(primes), 10) / math.log(primes[i], 10))
+        else:
+            check = False
+    result = result * primes[i] ^ exps[i]
+    i += 1
+print(result)
