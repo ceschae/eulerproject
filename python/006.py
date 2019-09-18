7e5946adbca4bd90383ca84f54bb5fb60385f076
Caitlin Schaefer <ceschae@gmail.com>
2019-09-17 17:03:18 -0700

    my solution

--- python/006.py
+++ python/006.py
@@ -1,3 +1,10 @@
 n = 100
 
-sumSquared = (((n + 1) * n) // 2) ^ 2
+sumSquared = (((n + 1) * n) // 2) ** 2
+
+squaredSum = 0
+for i in range(1, n + 1):
+    squaredSum += i ** 2
+
+result = sumSquared - squaredSum
+print(result)
