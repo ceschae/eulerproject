eeabaf1e8d559b993617dcdcefa3562ec5660e60
Caitlin Schaefer <ceschae@gmail.com>
2019-09-17 17:06:29 -0700

    better solution

--- python/006.py
+++ python/006.py
@@ -2,9 +2,7 @@ n = 100
 
 sumSquared = (((n + 1) * n) // 2) ** 2
 
-squaredSum = 0
-for i in range(1, n + 1):
-    squaredSum += i ** 2
+squaredSum = (2 * n + 1) * (n + 1) * n // 6
 
 result = sumSquared - squaredSum
 print(result)
